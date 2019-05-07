8bb1a2afa97b1c92e762f1cfc1492b2f31e8fb43
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-07 22:31:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+names_array = []
+array_hashes = []
+temp_hash_attr = {}
+hash = {}
+idx = 0
+
+data[:gender].each do |k, v|
+    data[:gender][k].each do |name|
+      names_array.push(name)
+    end
+  end
+
+names_array.each do |name_key |
+    hash[name_key] = {}
+  data.keys.each do |k |
+    temp_hash_attr[k] = []
+  end
+
+idx = 0
+while (idx < data.keys.size)
+  data.keys.each do |k|
+    data.values[idx].each do |attribute, names_arr |
+      if names_arr.include?(name_key)
+temp_hash_attr[k].push(attribute.to_s)
+end
+end
+
+idx += 1
+end
+end
+hash[name_key] = hash[name_key].merge(temp_hash_attr)
+
+end
+return hash
 end
