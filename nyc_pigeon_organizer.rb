5667cd27201fc59222a3a8351b9b001a7b051e18
keithburgie <krburgie@gmail.com>
2019-05-09 21:08:34 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+
+  data.each do |labels, values|
+    values.each do |symbols, names|
+      names.each do |name|
+        unless pigeon_hash.include?(name)
+          pigeon_hash[name] = {:color => [], :gender => [], :lives => []}
+        end
+      end
+    end
+  end
+  pigeon_hash.each do |name, hashes|
+    hashes.each do |hash_key, pigeons|
+      data.each do |labels, values|
+        values.each do |value, array|
+          array.each do |pigeon|
+            if pigeon == name && hash_key == labels
+              pigeon_hash[pigeon][hash_key] << value.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  pigeon_hash
 end
